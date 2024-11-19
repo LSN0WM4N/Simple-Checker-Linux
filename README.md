d042ec39f98205e55282bd4f95f086a26bbb5898
LSN0WM4N <rodriguezrondafrancis@gmail.com>
2024-11-19 07:46:33 -0500

    V 1.1 - Allow Subtasks

--- README.md
+++ README.md
@@ -20,6 +20,9 @@ As example:
 
 The folder you must pass as parameter is ```/my-pc/home/my-user/ProblemTestCases```
 
+## Subtsks 
+The checker allows subtasks if they are specified in the input filename, if you want to use subtasks, your file name must have the following format ``` <name>.<subtask>.<subtask_value> ```, if the test case ``` 004 ``` is in the second subtask, and the second subtask value is 35 points, the file must have the name ``` 004.2.35 ```.
+
 ## How to use
 
 Just execute it as other python file: 
--- checker.py
+++ checker.py
@@ -1,64 +1,6 @@
 import os
-import time
-import filecmp
-
-def compile_and_check(sourceCodePath):
-    if not (os.path.isfile(sourceCodePath + '.cpp')):
-        print('[-] Source file does not exist')
-        exit(1)
-
-    os.system(f"g++ {sourceCodePath}.cpp -o {sourceCodePath}.o -Wall")
-
-    if not (os.path.isfile(sourceCodePath + '.o')):
-        print('[-] Compiling error')
-        exit(1)
-
-    print('[+] Compiled Succefully')
-
-def test_all_cases(TestCasesFolder, sourceCode):
-    IFolder = TestCasesFolder + '/input'
-    OFolder = TestCasesFolder + '/output'
-
-    OFiles = os.listdir(OFolder)
-    IFiles = os.listdir(IFolder)
-
-    ac = 0
-    total = 0
-    worstTime = 0
-
-    for ifile in IFiles: 
-        ifile = ifile.split('.')
-        ifile = ifile[0]
-        if not f"{ifile}.out" in OFiles:
-            continue
-
-        prevTime = time.time()
-        returnValue = os.system(f"cat {IFolder}/{ifile}.in | timeout 1s {sourceCode}.o >| /tmp/ofiles/{ifile}.out")
-        nextTime = time.time()
-
-        if (nextTime - prevTime > 1):
-            print('[+] Time limit exeded')
-            os.remove(f'{sourceCode}.o')
-            exit(1)
-        if (returnValue != 0):
-            print('[-] Runtime Error')
-            os.remove(f'{sourceCode}.o')
-            exit(1)
-
-        worstTime = max(worstTime, nextTime - prevTime)
-        result = filecmp.cmp(f"/tmp/ofiles/{ifile}.out", f"{OFolder}/{ifile}.out")
-
-        total += 1
-        if result == True:
-            ac += 1
-
-    if ac == 0:
-        print(f"[-] Wrong Answer, 0 de {total}")
-    else:
-        print(f"Score: {(100 / total * ac):.2f}, {ac} of {total} [{worstTime:.3f}s]")
-    os.remove(f'{sourceCode}.o')
-
-    return [total, ac, worstTime]
+import compiler
+import tester
 
 def Execute(Folder, SourceCode): 
     try:
@@ -66,11 +8,10 @@ def Execute(Folder, SourceCode):
     except Exception as e:
         pass
 
-    compile_and_check(SourceCode)
-    test_all_cases(Folder, SourceCode)
+    compiler.Compile(SourceCode)
+    tester.RunTests(Folder, SourceCode)
 
-def AutomChecker():
-    Folder = input()
+def AutomChecker(Folder):
     SourceCode = Folder + '/main'
     Execute(Folder, SourceCode)
 
